a2b071445d753d5ff33320eca561ebf323e6bb57
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-22 22:19:51 +0530

    logic complete

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let imgBox = document.querySelector(".img-box")
 let imgWrap = document.querySelector(".img-wrap")
+let originalImg = document.getElementById("originalImg")
+let line = document.getElementById("line")
+originalImg.style.width = imgBox.offsetWidth + "px"
 
-imgBox.onmousemove = function
+var leftSpace = imgBox.offsetLeft;
+
+imgBox.onmousemove = function(e){
+    var boxWidth = (e.pageX - leftSpace) + "px";
+    imgWrap.style.width = boxWidth  
+    line.style.left = boxWidth
+}
